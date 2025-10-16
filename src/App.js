7ea3f7179e0c5b5f7385a8c02e5083c2c188f59b
TatiGS <tatiggarcia@gmail.com>
2025-10-16 20:26:21 -0300

    Contact Form e atualização do App.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import react, {useState} from "react";
+import React, {useState, useEffect} from "react";
 import  './App.css';
 
 //importa os componentes de seus rescpectivos arquivos
@@ -11,7 +11,7 @@ import ContactForm from "./ContactForm/ContactForm";
 function App(){
 
   const [isLoggedIn, setIsLoggedIn] = useState(false);
-  const [activeScreen, setActiveScreen] = useState('Welcome');
+  const [activeScreen, setActiveScreen] = useState('welcome');
   const [contacts, setContacts] = useState([
     {id:1, name:'Tatiana Silva', phone: '1621068702'},
     {id:2, name: 'Graziela Carozelli', phone:'1621068713'}]);
@@ -29,7 +29,18 @@ function App(){
       }
     };
 
-    const handleSaveContact = (contact) => {}
+    const handleSaveContact = (contact) => {
+      if(contact.id){
+        //se o contact.id tem um valoe então é uma atualização
+        setContacts(contacts.map(c => c.id === contact.id ? contact: c));
+        alert("Contato alterado com sucesso!")
+      }else{
+        contact.id = Date.now();
+        setContacts([...contacts, contact]);
+        alert("Conato cadastrado com sucesso");
+      }
+    }
+
     const handleDeleteContact = (id) => {}
     const startEdit = (contact) => {}
     const showCreatForm = () => {}
@@ -40,10 +51,10 @@ function App(){
       return <Login onLogin={handleLogin}/>
     }
     return(
-      <div>
+      <div className="app-container">
         <Menu onNavigate={handleNavigate} onCreate={showCreatForm}/>
         <main className='content'>
-          {activeScreen ==='Welcome' && <Welcome/>}
+          {activeScreen ==='welcome' && <Welcome/>}
           {activeScreen === 'list' && <ContactList contacts={contacts}
                             onEdit={startEdit}
                             onDelete={handleDeleteContact}/>}
--- src/ContactForm/ContactForm.js
+++ src/ContactForm/ContactForm.js
@@ -1,9 +1,52 @@
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import './ContactForm.css';
 
 function ContactForm({oneSave, contactToEdit}){
+    const[name, setName] = useState('');
+    const[phone, setPhone] = useState('');
+
+    useEffect(()=>{
+        if(contactToEdit){
+//se contactToEdit é diferente de null 
+    setName(contactToEdit.name);
+    setPhone(contactToEdit.phone);
+ 
+        }else{
+            setName('');
+            setPhone('');
+        }
+        
+    },[contactToEdit])
+
+    const handleSubmit= (e) =>{
+        e.preventDefault();
+        if(!name || !phone){
+            alert('Por favor, preencha nome e telefone')
+            return;
+        }
+        oneSave({id:contactToEdit ? contactToEdit.id: null,  name, phone})
+    }
+
+    
     return(
-        <div>
+        <div className="form-container">
+            <h2>{contactToEdit ? 'Alterar Contato': 'Cadastrar Contato'}</h2>
+            <form onSubmit={handleSubmit}>
+            <input
+            type="text"
+             placeholder="Nome"
+             value={name}
+             onChange={(e)=> setName(e.target.value)}
+             />
+               <input
+            type="text"
+             placeholder="Telefone"
+             value={phone}
+             onChange={(e)=> setPhone(e.target.value)}
+             />
+             <button type="submit">Salvar</button>
+            </form>
+        
 
 
         </div>
